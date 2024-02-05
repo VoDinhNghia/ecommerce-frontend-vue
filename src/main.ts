f1572cd7aa32fcf5ccb6f77933cd8beedc04fe37
VoDinhNghia <vodinhnghia85@gmail.com>
2024-02-05 20:41:41 +0700

    config project

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import bootstrapvue3 from 'bootstrap-vue-3'
+
+// since every component imports their bootstrap functionality,
+// the following line is not necessary:
+// import 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(bootstrapvue3)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import MenuHome from '../components/MenuHome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <MenuHome />
   </main>
 </template>
