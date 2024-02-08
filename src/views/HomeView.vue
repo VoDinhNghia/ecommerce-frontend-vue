2a37cf253958059bbce06df8719bc02c9b3e8951
VoDinhNghia <vodinhnghia85@gmail.com>
2024-02-08 19:57:25 +0700

    footer page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import MenuHome from '../components/MenuHome.vue'
+import FooterPage from '../components/FooterPage.vue'
 </script>
 
 <template>
   <MenuHome />
+  <FooterPage />
 </template>
