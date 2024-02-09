ed14b1741a57c961e70174063966fe88bef629d9
VoDinhNghia <vodinhnghia85@gmail.com>
2024-02-09 19:47:39 +0700

    write UI login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,7 +5,38 @@ import FooterPage from '../components/FooterPage.vue'
 <template>
   <div>
     <MenuHomePage />
-    <h1>Login page</h1>
+    <div class="LoginPageMain container-fluid mt-2 mb-2">
+      <div class="row no-gutter">
+        <div class="col-md-6 d-none d-md-flex bg-image"></div>
+        <div class="col-md-6 bg-light">
+          <div class="LoginPage d-flex align-items-center py-5">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-10 col-xl-7 mx-auto">
+                  <h3 class="display-4 text-center mb-3">Login</h3>
+                  <form>
+                    <div class="mb-3">
+                      <input id="inputEmail" type="email" placeholder="Email address" required="true" autofocus="true"
+                        class="form-control rounded-pill border-0 shadow-sm px-4" />
+                    </div>
+                    <div class="mb-3">
+                      <input id="inputPassword" type="password" placeholder="Password" required="true"
+                        class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
+                    </div>
+                    <div class="d-grid gap-2 mt-2">
+                      <button type="submit"
+                        class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <FooterPage />
   </div>
 </template>
+
+<style>@import "../assets/login.css";</style>
